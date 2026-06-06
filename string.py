20becf473ff6d986fef9d982cafd314fa866a7a4
praveena-pawar <praveena.dev02@gmail.com>
2026-06-06 20:09:37 +0530

    solved question 1

--- string.py
+++ string.py
@@ -1,1 +1,9 @@
 # 1: Write a function count_characters(s) that returns the total number of characters in a string without using len().
+def count_characters(word):
+    total = 0
+
+    for ch in word:
+        total += 1
+
+    return total
+print(count_characters("python"))
