0576e379b1a80f517156dcdf679c3c9bdea45c73
praveena-pawar <praveena.dev02@gmail.com>
2026-06-19 11:11:13 +0530

    solved problem 2

--- list.py
+++ list.py
@@ -13,3 +13,14 @@ print(list_sum([10, 20, 30, 40]))
 
 
 # 2: Find the Smallest Element
+def find_smallest(nums):
+    small_element = nums[0]
+
+    for i in nums:
+        if i < small_element:
+            small_element = i
+
+    return small_element
+
+
+print(find_smallest([8, 3, 12, 1, 5]))
