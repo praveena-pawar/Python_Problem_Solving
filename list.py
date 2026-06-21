d61fe998a02c7da7ba608a6d15db0ca93ef51534
praveena-pawar <praveena.dev02@gmail.com>
2026-06-21 10:52:45 +0530

    solved problem 16

--- list.py
+++ list.py
@@ -224,3 +224,11 @@ print(rotate_right([1, 2, 3, 4, 5]))
 
 
 # 16: Check if a List Is Sorted
+def is_sorted(nums):
+    for i in range(len(nums)-1):
+        if nums[i] > nums[i + 1]:
+            return False
+        
+    return True
+
+print(is_sorted([1, 2, 3, 4, 5]))
