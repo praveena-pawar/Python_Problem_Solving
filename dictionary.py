6b6d529a726f793c85236e6d44695c64cbde0d50
praveena-pawar <praveena.dev02@gmail.com>
2026-06-25 21:18:18 +0530

    solved problem 3

--- dictionary.py
+++ dictionary.py
@@ -38,5 +38,21 @@ print(most_frequent([1, 2, 2, 3, 1, 2]))
 
 
 # 3: Find All Duplicate Elements
+def find_duplicates(nums):
+    freq = {}
 
+    for i in nums:
+        if i in freq:
+            freq[i] += 1
+        else:
+            freq[i] = 1
+
+    uniq = []
+    for key in freq:
+        if freq[key] > 1:
+            uniq.append(key)
+
+    return uniq
+
+print(find_duplicates([1, 2, 2, 3, 1, 4, 5, 5]))
 
