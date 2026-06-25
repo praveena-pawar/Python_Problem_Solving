a638189d348173ab8761ee52d10f339b40736ad0
praveena-pawar <praveena.dev02@gmail.com>
2026-06-25 20:57:44 +0530

    solved problem 1

--- dictionary.py
+++ dictionary.py
@@ -1,1 +1,13 @@
 # 1: Count Frequency of Elements
+def count_frequency(nums):
+    freq = {}
+
+    for i in nums:
+        if i in freq:
+            freq[i] += 1
+        else:
+            freq[i] = 1
+
+    return freq
+
+print(count_frequency([1, 2, 2, 3, 1, 2]))
