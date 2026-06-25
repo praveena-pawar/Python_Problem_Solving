793dcde8abd26cbe1edac98e1b10a9ec8b435e8e
praveena-pawar <praveena.dev02@gmail.com>
2026-06-25 21:11:44 +0530

    solved problem 2

--- dictionary.py
+++ dictionary.py
@@ -15,3 +15,22 @@ print(count_frequency([1, 2, 2, 3, 1, 2]))
 
 
 # 2: Find the Most Frequent Element
+def most_frequent(nums):
+    freq = {}
+
+    for i in nums:
+        if i in freq:
+            freq[i] += 1
+        else:
+            freq[i] = 1
+
+    highest_freq = 0
+    most_frequent_element = None
+    for key in freq:
+        if freq[key] > highest_freq:
+            highest_freq = freq[key]
+            most_frequent_element = key
+
+    return most_frequent_element
+
+print(most_frequent([1, 2, 2, 3, 1, 2]))
