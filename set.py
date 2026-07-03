b97ef55b6f2dba996b36695a3718aa62f6377e43
praveena-pawar <praveena.dev02@gmail.com>
2026-07-03 18:41:51 +0530

    solved problem 2

--- set.py
+++ set.py
@@ -12,3 +12,15 @@ print(remove_duplicates([1, 2, 2, 3, 1, 4, 3]))
 
 
 # 2: Check if a List Has Duplicates
+def has_duplicates(nums):
+    unique_num = set()
+
+    for i in nums:
+        if i in unique_num:
+            return True  
+        else:
+            unique_num.add(i)  
+
+    return False
+      
+print(has_duplicates([1, 2, 3, 2]))
