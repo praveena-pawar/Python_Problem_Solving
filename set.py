8b687cb30bc504e34c1b079dda945becc7fc38ed
praveena-pawar <praveena.dev02@gmail.com>
2026-07-03 18:30:14 +0530

    solved problem 1

--- set.py
+++ set.py
@@ -1,1 +1,10 @@
 # 1: Remove Duplicates
+def remove_duplicates(nums):
+    unique_num = set()
+
+    for i in nums:
+            unique_num.add(i)
+
+    return unique_num
+
+print(remove_duplicates([1, 2, 2, 3, 1, 4, 3]))
