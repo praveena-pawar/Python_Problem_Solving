718febb883e070507ddb9fa1b16bc74ce17e178b
praveena-pawar <praveena.dev02@gmail.com>
2026-07-06 10:57:17 +0530

    Add solutions for 15 set problems

--- set.py
+++ set.py
@@ -284,3 +284,22 @@ print(unique_to_each_list(
 
 
 # 15: Longest Consecutive Sequence
+def longest_consecutive(nums):
+    num_set = set(nums)
+    longest = 0
+
+    for num in num_set:
+        if num - 1 not in num_set:
+            current = num
+            length = 1
+
+            while current + 1 in num_set:
+                current += 1
+                length += 1
+
+            if length > longest:
+                longest = length
+
+    return longest
+
+print(longest_consecutive([100, 4, 200, 1, 3, 2]))
