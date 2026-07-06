b0e521eb201e7ee868c692758b5c36d5a021bcac
praveena-pawar <praveena.dev02@gmail.com>
2026-07-06 11:03:17 +0530

    solved problem 1

--- tuple.py
+++ tuple.py
@@ -1,1 +1,16 @@
 # 1: Find the Second Largest Distinct Element
+def second_largest(t):
+    largest = float('-inf')
+    second = float('-inf')
+
+    for i in t:
+        if i > largest:
+            largest = i
+
+    for i in t:
+        if i < largest and i > second:
+            second = i
+
+    return second
+
+print(second_largest((10, 5, 20, 20, 8, 15)))
